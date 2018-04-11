c1c9e389e640cca6d8da1d1740180f6f4a8a58d1
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-04-11 11:08:29 -0700

    Add Second, Test Scene

---
+++ Foxmomma/Assets/TempPlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayerMovement : MonoBehaviour {
+    public float walkSpeed, sprintSpeed, crouchSpeed;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Horizontal") < 0 && Input.GetAxisRaw("Vertical") < 0) ;
+	}
+}
