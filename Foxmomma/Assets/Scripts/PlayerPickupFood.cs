a46b8908107424b19568895fdbb00aa43ae59dae
BMarcelus <bmdizon@uci.edu>
2018-05-30 11:13:44 -0700

    integration with scene

--- Foxmomma/Assets/Scripts/PlayerPickupFood.cs
+++ Foxmomma/Assets/Scripts/PlayerPickupFood.cs
@@ -11,16 +11,18 @@ public class PlayerPickupFood : MonoBehaviour {
   private GameObject pickUpObject;
   private bool holding = false;
   private bool canPickUp = false;
+  private LayerMask foodLayer;
+  private LayerMask pupsLayer;
 	// Use this for initialization
 	void Start () {
-
+    foodLayer = LayerMask.NameToLayer("Food");
+    pupsLayer = LayerMask.NameToLayer("Pups");
 	} 
 	
 	// Update is called once per frame
 	void Update () {
 		if(holding && Input.GetKeyDown(dropKey)) {
-      held.transform.parent = transform.parent;
-      holding = false;
+      drop(held);
     } else if(!holding&&canPickUp&&Input.GetKeyDown(pickUpKey)) {
       pickUp(pickUpObject);
     }
@@ -34,6 +36,11 @@ public class PlayerPickupFood : MonoBehaviour {
     canPickUp = false;
   }
 
+  void drop(GameObject obj) {
+    obj.transform.parent = transform.parent;
+    holding = false;
+  }
+
   void OnTriggerExit(Collider col) {
     GameObject obj = col.gameObject;
     if (obj.GetInstanceID() == pickUpObject.GetInstanceID()) {
@@ -43,15 +50,17 @@ public class PlayerPickupFood : MonoBehaviour {
 
   void OnTriggerEnter(Collider col) {
     GameObject obj = col.gameObject;
-    if(obj.layer == LayerMask.NameToLayer("Food")) {
+    if(obj.layer == foodLayer) {
       pickUpObject = obj;
       canPickUp = true;
+    } else if(holding && obj.layer == pupsLayer) {
+      drop(held);
     }
   }
 
   void OnTriggerStay(Collider col) {
     GameObject obj = col.gameObject;
-    if(!canPickUp && obj.layer == LayerMask.NameToLayer("Food")) {
+    if(!canPickUp && obj.layer == foodLayer) {
       pickUpObject = obj;
       canPickUp = true;
     }
